14a6c494065ac7e0dfb2c42127b355c14533ed3d
Chiel Timmermans <chiel1997@hotmail.com>
2019-03-04 19:57:48 +0100

    removed debug code

--- SkereBiertjes/BeerScraper.cs
+++ SkereBiertjes/BeerScraper.cs
@@ -169,11 +169,6 @@ namespace SkereBiertjes
             };
             this.databaseHandler.store(beers);
             this.beers = this.databaseHandler.get();
-            Debug.WriteLine(this.beers.Count());
-            this.databaseHandler.delete();
-            this.databaseHandler.store(beers);
-            this.beers = this.databaseHandler.get();
-            Debug.WriteLine(this.beers.Count());
         }
 
         public Scraper Scraper
