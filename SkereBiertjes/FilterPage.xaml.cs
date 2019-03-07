66602632732800403aa23b842192aee48ef79713
Yosh Schoemaker <yosh.schoemaker@aceworks.nl>
2019-03-07 13:36:50 +0100

    Filter page UI done I guess

--- SkereBiertjes/FilterPage.xaml.cs
+++ SkereBiertjes/FilterPage.xaml.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SkereBiertjes
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Filter page. Used for filtering search results on Shop, Brand and Type.
     /// </summary>
     public sealed partial class FilterPage : Page
     {
@@ -16,6 +17,30 @@ namespace SkereBiertjes
         public FilterPage()
         {
             this.InitializeComponent();
+        }
+
+        // Fired when the Save button is clicked (Bottom of the page)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Fired when the Shop selection changes
+        private void ShopComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // e.AddedItems[0].ToString(); <-- Get the current selection
+        }
+
+        // Fired when the Brand selection changes
+        private void BrandComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        // Fired when the Type selection changes
+        private void TypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
 
     }
