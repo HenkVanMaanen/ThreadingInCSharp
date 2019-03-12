224e027e5042a3f24e9777df8c8a4695570e90d6
Chiel Timmermans <chiel1997@hotmail.com>
2019-03-12 13:57:31 +0100

    added image to Beer, added comments

--- SkereBiertjes/GallEnGallScraper.cs
+++ SkereBiertjes/GallEnGallScraper.cs
@@ -16,67 +16,85 @@ namespace SkereBiertjes
 
         public GallEnGallScraper()
         {
+            //set data
             beers = new List<Beer>();
             StandardURL = @"Data/gall&gall.html";
         }
 
         string Scraper.getHTML()
         {
+            return null;
+        }
+
+
+        List<Beer> Scraper.parseHTML()
+        {
+            List<Beer> beers = new List<Beer>();
+            //get document
             var doc = new HtmlDocument();
             doc.OptionFixNestedTags = true;
             doc.Load(StandardURL);
 
+            //select all articles
             var nodes = doc.DocumentNode.SelectNodes("//article[contains(@class,'product-block')]");
 
+            //check if nodes are not null
             if (nodes == null)
             {
                 Debug.WriteLine("No nodes selected");
                 return null;
             }
+            //check if document node is not null
             if (doc.DocumentNode == null)
             {
                 Debug.WriteLine("DocumentNode == null");
                 return null;
             }
 
+            //loop over all articles
             foreach (var node in nodes)
             {
+                //if node is null skip it
                 if (node != null)
                 {
-                    HtmlNodeCollection node2 = node.SelectNodes(".//img[contains(@itemprop,'image')]");
-                    Debug.WriteLine(node2["img"].Attributes["src"].Value);
+                    //parse data from node
+                    Beer beer = this.parseData(node);
+                    beers.Add(beer);
 
-                    foreach (var dataTrackingImpression in node.Attributes)
-                    {
-                        if (dataTrackingImpression.Name == "data-tracking-impression")
-                        {
-                            JObject json = JObject.Parse(dataTrackingImpression.Value);
-                            Beer beer = this.parseData(json);
-                            beer.printInfo();
-                        }
-                    }
-
+                    //print info from beer
+                    beer.printInfo();
                 }
             }
-            return null;
+            return beers;
         }
-         
-        private Beer parseData(JObject json)
+
+        //parse the article node to a beer
+        private Beer parseData(HtmlNode node)
         {
+            //get the node where the image is
+            HtmlNodeCollection imageNode = node.SelectNodes(".//img[contains(@itemprop,'image')]");
+            string ImageURL = "";
+
+            //check if is not empty
+            if (imageNode != null)
+            {
+                ImageURL = imageNode["img"].Attributes["src"].Value;
+            }
+
+            //parse to json
+            JObject json = JObject.Parse(node.Attributes["data-tracking-impression"].Value);
+
+            //parse to usable data
             string name = json["name"].ToString();
             int volume = this.parseNameToVolume(name);
             int price = Convert.ToInt32(Math.Round(Convert.ToDouble(json["price"].ToString()) * 100));
             string discount = "";
-            string url = "";
-            Beer beer = CreateBeer(name, volume, price, discount, url);
+
+            //create beer
+            Beer beer = CreateBeer(name, volume, price, discount, ImageURL);
 
 
             return beer;
-        }
-
-        List<Beer> Scraper.parseHTML()
-        {
-            throw new NotImplementedException();
         }
 
         public List<Beer> getBeers()
@@ -84,11 +102,13 @@ namespace SkereBiertjes
             return beers;
         }
 
+        //create a beer with Gall&Gall allready in it;
         private Beer CreateBeer(string brand, int volume, int priceNormalized, string discount, string url)
         {
             return new Beer(brand, volume, priceNormalized, discount, "Gall&Gall", url);
         }
 
+        //bc volume is only accisble in name, parse name to get volume in ml
         private int parseNameToVolume(string title)
         {
             if(title == "")
@@ -97,6 +117,8 @@ namespace SkereBiertjes
             }
 
             string[] words = title.Split(' ');
+
+            //will parse everything ending in CL
             if (words[words.Length - 1].Contains("CL"))
             {
                 string word = words[words.Length - 1];
@@ -104,8 +126,6 @@ namespace SkereBiertjes
                 return Convert.ToInt32(Math.Round(Convert.ToDouble(word) * 10)); 
             }
             return 0;
-
-
         }
     }
 }
