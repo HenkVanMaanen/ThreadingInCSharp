c7773a369c175f7e3ae459b9809baaf2078fbe4a
Aron Soppe <aronsoppe@hotmail.com>
2019-04-04 11:23:28 +0200

    Tried adding items to grid async

--- SkereBiertjes/MainPage.xaml.cs
+++ SkereBiertjes/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -18,6 +19,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -40,6 +43,7 @@ namespace SkereBiertjes
         {
             this.suggestions = new ObservableCollection<string>();
             this.InitializeComponent();
+            BigIcon.Text = SearchIcon;
         }
 
         private void BeerSearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
@@ -54,22 +58,22 @@ namespace SkereBiertjes
                 {
                     var possibleSuggestions = new ObservableCollection<string>();
                     possibleSuggestions.Clear();
-                    possibleSuggestions.Add("Albert Heijn");
-                    possibleSuggestions.Add("Jumbo");
-                    possibleSuggestions.Add("Gal & Gal");
-                    possibleSuggestions.Add("Coop");
-                    possibleSuggestions.Add("PLUS");
-                    possibleSuggestions.Add("Hertog Jan");
-                    possibleSuggestions.Add("Grolsch");
-                    possibleSuggestions.Add("Palm");
-                    possibleSuggestions.Add("Bavaria");
-                    possibleSuggestions.Add("Brand");
-                    possibleSuggestions.Add("Heineken");
-                    possibleSuggestions.Add("Jupiler");
-                    possibleSuggestions.Add("Kordaat");
-                    possibleSuggestions.Add("Kornuit");
-                    possibleSuggestions.Add("Amstel");
-                    possibleSuggestions.Add("Gulpener");
+                    //possibleSuggestions.Add("Albert Heijn");
+                    //possibleSuggestions.Add("Jumbo");
+                    //possibleSuggestions.Add("Gal & Gal");
+                    //possibleSuggestions.Add("Coop");
+                    //possibleSuggestions.Add("PLUS");
+                    //possibleSuggestions.Add("Hertog Jan");
+                    //possibleSuggestions.Add("Grolsch");
+                    //possibleSuggestions.Add("Palm");
+                    //possibleSuggestions.Add("Bavaria");
+                    //possibleSuggestions.Add("Brand");
+                    //possibleSuggestions.Add("Heineken");
+                    //possibleSuggestions.Add("Jupiler");
+                    //possibleSuggestions.Add("Kordaat");
+                    //possibleSuggestions.Add("Kornuit");
+                    //possibleSuggestions.Add("Amstel");
+                    //possibleSuggestions.Add("Gulpener");
 
                     foreach (string possibleSuggestion in possibleSuggestions)
                     {
@@ -90,20 +94,29 @@ namespace SkereBiertjes
             // Set sender.Text. You can use args.SelectedItem to build your text string.
         }
 
+        private void uiSearchMode()
+        {
+            EmptyStateElements.Visibility = Visibility.Collapsed;
+            BeerItemsGrid.Visibility = Visibility.Collapsed;
+            BigIcon.Text = SearchIcon;
+            InfoGrid.Opacity = 0d;
+        }
+
+        private void uiDoneSearchingResultEmpty()
+        {
+            BeerItemsGrid.Visibility = Visibility.Collapsed;
+            EmptyStateElements.Visibility = Visibility.Visible;
+            progressRing.IsActive = false;
+        }
 
         private async void BeerSearchBox_QuerySubmitted(AutoSuggestBox sender,
             AutoSuggestBoxQuerySubmittedEventArgs args)
         {
+            progressRing.IsActive = true;
+
             if (args.ChosenSuggestion == null)
             {
-                progressRing.IsActive = true;
-                EmptyStateElements.Visibility = Visibility.Collapsed;
-                BeerItemsGrid.Visibility = Visibility.Collapsed;
-                BigIcon.Text = SearchIcon;
-                InfoGrid.Opacity = 0d;
-                
-                // Disable progress ring animation
-                progressRing.IsActive = false;
+                uiSearchMode();
 
                 // Hide the Empty State Elements (Zoom icon)
                 EmptyStateElements.Visibility = Visibility.Collapsed;
@@ -112,89 +125,162 @@ namespace SkereBiertjes
                 if (!string.IsNullOrWhiteSpace(args.QueryText) && this.beerScraper.getBeersCount() > 0)
                 {
                     // Put beers in a new ArrayList for making up the grid
-                    this.displayBeersOnScreen(args.QueryText);
-
+                    await displayBeersOnScreenAsync(args.QueryText);
                 }
                 // No beers found
                 else if (this.beerScraper.getBeersCount() == 0)
                 {
-                    BeerItemsGrid.Visibility = Visibility.Collapsed;
-                    EmptyStateElements.Visibility = Visibility.Visible;
-
                     BigIcon.Text = ErrorIcon;
                     EmptyStateTextBlock.Text = "We hebben helaas niks kunnen vinden...";
+                    uiDoneSearchingResultEmpty();
                 }
                 // Nothing entered inside the SearchBox
                 else
                 {
-                    BeerItemsGrid.Visibility = Visibility.Collapsed;
-                    EmptyStateElements.Visibility = Visibility.Visible;
-
                     EmptyStateTextBlock.Text = "Je zoekopdracht is leeg...";
+                    uiDoneSearchingResultEmpty();
                 }
             }
         }
 
-        private void displayBeersOnScreen(string search)
+        private async Task displayBeersOnScreenAsync(string search)
         {
+            // Backend stuff is put in the Task below
+            await Task.Run(async () =>
+            {
+                List<object> beersGridCollection = new List<object>();
+                beers = this.beerScraper.search(search, this.filter);
+
+                foreach (var beer in beers)
+                {
+                    var description =
+                        $"{beer.getTitle()} - {beer.getBottleAmount()} x {(float) beer.getVolume() / 1000}L";
+                    var hasReduction = (string.IsNullOrWhiteSpace(beer.getDiscount()))
+                        ? Visibility.Collapsed
+                        : Visibility.Visible;
+                    float.TryParse(beer.getDiscount(), out var discount);
+
+
+                    beersGridCollection.Add(
+                        new
+                        {
+                            BeerDescription = description,
+                            ReductionPriceVisibility = hasReduction,
+                            OriginalPrice = $"€ {String.Format("{0:0.00}", Convert.ToDecimal(discount) / 100)}",
+                            Price =
+                                $"€ {String.Format("{0:0.00}", Convert.ToDecimal(beer.getNormalizedPrice()) / 100)}",
+                            ImageUrl = beer.getUrl(),
+                            ShopImageUrl = $"/Assets/shop/{beer.getShopName().Replace(" ", "").ToLower()}.png",
+                        });
+
+                    //Debug.WriteLine(beer.getTitle());
+                }
+
+                // UI Work is done below
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    BeerItemsGrid.Visibility = Visibility.Visible;
+                    BeerItemsGrid.ItemsSource = beersGridCollection;
+                    progressRing.IsActive = false;
+                });
+            });
+
+            /*var beerItemsSource = new ArrayList();
+
             // Start the stopwatch and start getting beers
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            
-            List<Beer> beers = this.beerScraper.search(search, this.filter);
-            EmptyStateElements.Visibility = Visibility.Collapsed;
-            
-            // Stop stopwatch and get the elapsed time
-            stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
 
-            var beerItemsSource = new ArrayList();
-            foreach (var beer in beers)
+            List<Beer> beers = new List<Beer>();
+            try
             {
-                var description =
-                    $"{beer.getTitle()} - {beer.getBottleAmount()} x {(float)beer.getVolume() / 1000}L";
-                var hasReduction = (string.IsNullOrWhiteSpace(beer.getDiscount()))
-                    ? Visibility.Collapsed
-                    : Visibility.Visible;
-                float.TryParse(beer.getDiscount(), out var discount);
+                var getBeersTask = Task<List<Beer>>.Factory.StartNew(() =>
+                {
+                    beers = this.beerScraper.searchAsync(search, this.filter).Result;
 
-
-                beerItemsSource.Add(
-                    new
+                    foreach (var beer in beers)
                     {
-                        BeerDescription = description,
-                        ReductionPriceVisibility = hasReduction,
-                        OriginalPrice = $"€ {String.Format("{0:0.00}", Convert.ToDecimal(discount) / 100)}",
-                        Price = $"€ {String.Format("{0:0.00}", Convert.ToDecimal(beer.getNormalizedPrice()) / 100)}",
-                        ImageUrl = beer.getUrl(),
-                        ShopImageUrl = $"/Assets/shop/{beer.getShopName().Replace(" ", "").ToLower()}.png",
-                    });
+                        var description =
+                            $"{beer.getTitle()} - {beer.getBottleAmount()} x {(float) beer.getVolume() / 1000}L";
+                        var hasReduction = (string.IsNullOrWhiteSpace(beer.getDiscount()))
+                            ? Visibility.Collapsed
+                            : Visibility.Visible;
+                        float.TryParse(beer.getDiscount(), out var discount);
 
+                        beerItemsSource.Add(
+                            new
+                            {
+                                BeerDescription = description,
+                                ReductionPriceVisibility = hasReduction,
+                                OriginalPrice = $"€ {String.Format("{0:0.00}", Convert.ToDecimal(discount) / 100)}",
+                                Price =
+                                    $"€ {String.Format("{0:0.00}", Convert.ToDecimal(beer.getNormalizedPrice()) / 100)}",
+                                ImageUrl = beer.getUrl(),
+                                ShopImageUrl = $"/Assets/shop/{beer.getShopName().Replace(" ", "").ToLower()}.png",
+                            });
+                    }
 
+                    return beers;
+                });
+
+                // Continue
+                await getBeersTask.ContinueWith(async t =>
+                {
+                    try
+                    {
+                        await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            //List<Beer> beers = new List<Beer>();
+                            EmptyStateElements.Visibility = Visibility.Collapsed;
+
+                            if (beers.Count == 0)
+                            {
+                                BeerItemsGrid.Visibility = Visibility.Collapsed;
+                                EmptyStateElements.Visibility = Visibility.Visible;
+
+                                //BigIcon.Text = ErrorIcon;
+                                EmptyStateTextBlock.Text = "We hebben helaas niks kunnen vinden...";
+                                stopWatch.Stop();
+                            }
+                            else
+                            {
+                                // Show the grid
+                                BeerItemsGrid.Visibility = Visibility.Visible;
+
+                                // Add the beerItemsSource to the ItemsSource for the ItemsControl in the Xaml
+                                BeerItemsGrid.ItemsSource = beerItemsSource;
+
+                                // Display search time and amount of results
+                                InfoGrid.Opacity = 100d;
+                                // Stop stopwatch and get the elapsed time
+                                stopWatch.Stop();
+                                TimeSpan ts = stopWatch.Elapsed;
+                                var secondsText = ((double) ((ts.Seconds * 1000) + ts.Milliseconds) / 1000 == 1d)
+                                    ? "seconde"
+                                    : "seconden";
+                                var resultsText = (beers.Count == 1) ? "resultaat" : "resultaten";
+                                TimingResults.Text =
+                                    $"{beers.Count} {resultsText} in {(double) ((ts.Seconds * 1000) + ts.Milliseconds) / 1000} {secondsText}";
+                            }
+
+                            // Disable progress ring animation
+                            progressRing.IsActive = false;
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.StackTrace);
+                        Debug.WriteLine(ex.Message);
+                        Debug.WriteLine("CAUGHT UI THREAD EXCEPTION");
+                    }
+                });
             }
-
-
-            if (beers.Count == 0)
+            catch (Exception e)
             {
-                BeerItemsGrid.Visibility = Visibility.Collapsed;
-                EmptyStateElements.Visibility = Visibility.Visible;
-
-                BigIcon.Text = ErrorIcon;
-                EmptyStateTextBlock.Text = "We hebben helaas niks kunnen vinden...";
-            } else
-            {
-                // Show the grid
-                BeerItemsGrid.Visibility = Visibility.Visible;
-
-                // Add the beerItemsSource to the ItemsSource for the ItemsControl in the Xaml
-                BeerItemsGrid.ItemsSource = beerItemsSource;
-
-                // Display search time and amount of results
-                InfoGrid.Opacity = 100d;
-                var secondsText = ((double)((ts.Seconds * 1000) + ts.Milliseconds) / 1000 == 1d) ? "seconde" : "seconden";
-                var resultsText = (beers.Count == 1) ? "resultaat" : "resultaten";
-                TimingResults.Text = $"{beers.Count} {resultsText} in {(double)((ts.Seconds * 1000) + ts.Milliseconds) / 1000} {secondsText}";
-            }
+                Debug.WriteLine(e.StackTrace);
+                Debug.WriteLine(e.Message);
+                Debug.WriteLine("CAUGHT NORMAL THREAD EXCEPTION");
+            }*/
         }
 
         private void EmptyStateTextBlock_Loaded(object sender, RoutedEventArgs e)
@@ -214,16 +300,24 @@ namespace SkereBiertjes
             EmptyStateTextBlock.Text = searchTextArray[rnd_num];
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            if(e.Parameter is IDictionary<string, Object>)
+            progressRing.IsActive = true;
+
+            if (e.Parameter is IDictionary<string, Object>)
             {
                 IDictionary<string, Object> data = (IDictionary<string, Object>) e.Parameter;
                 this.filter = (Filter) data["filter"];
                 this.beerScraper = (BeerScraper) data["beerScraper"];
-                
-                this.displayBeersOnScreen("");
+
+                uiSearchMode();
+                //var dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+                //await dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                //    () => { displayBeersOnScreen(""); });
+
+                await displayBeersOnScreenAsync("");
             }
+
             base.OnNavigatedTo(e);
         }
     }
